e569aed0a666aaa28f326e647529b8ddab6ac49b
Alfonso <fonxi1994@gmail.com>
2018-08-07 21:47:49 +0200

    New featured start

--- page-home.php
+++ page-home.php
@@ -18,77 +18,7 @@ get_header();
 ?>
 
 <section class="d-none d-lg-block" id="featured">
-	<div class="container">
-	<?php if ( have_rows( 'fila_1' ) ) : ?>
-	<?php while ( have_rows( 'fila_1' ) ) : the_row(); ?>
-	<div class="row featured-row">
-	  <div class="col-md-7 text-center">
-	  	<?php if ( get_sub_field( 'imagen_1' ) ) { ?>
-	    <div class="feat-article"><img class="img-fluid feat-img" src="<?php the_sub_field( 'imagen_1' ); ?>" alt="<?php the_sub_field( 'titulo_1' ); ?>"/>
-	    <?php } ?>
-	      <div class="hover-featured">
-	      	<?php $enlace_1 = get_sub_field( 'enlace_1' ); ?>
-	      	<?php if ( $enlace_1 ) { ?>
-	        <div class="featured-text"><a href="<?php echo $enlace_1; ?>">
-	            <h2><?php the_sub_field( 'titulo_1' ); ?></h2></a>
-	        <?php } ?>
-	        </div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-md-5 text-center">
-	  	<?php if ( get_sub_field( 'imagen_2' ) ) { ?>
-	    <div class="feat-article"><img class="img-fluid feat-img" src="<?php the_sub_field( 'imagen_2' ); ?>" alt="<?php the_sub_field( 'titulo_2' ); ?>"/>
-	    <?php } ?>
-	    	<div class="hover-featured">
-	      <?php $enlace_2 = get_sub_field( 'enlace_2' ); ?>
-	      	<?php if ( $enlace_2 ) { ?>
-	        <div class="featured-text"><a href="<?php echo $enlace_2; ?>">
-	            <h2><?php the_sub_field( 'titulo_2' ); ?></h2></a>
-	        <?php } ?>
-	        </div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
-	<?php endwhile; ?>
-	<?php endif; ?>
-
-	<?php if ( have_rows( 'fila_2' ) ) : ?>
-	<?php while ( have_rows( 'fila_2' ) ) : the_row(); ?>
-	<div class="row featured-row">
-	  <div class="col-md-5 text-center">
-	  	<?php if ( get_sub_field( 'imagen_3' ) ) { ?>
-	    <div class="feat-article"><img class="img-fluid feat-img" src="<?php the_sub_field( 'imagen_3' ); ?>" alt="<?php the_sub_field( 'titulo_3' ); ?>"/>
-	    <?php } ?>
-	      <div class="hover-featured">
-	        <?php $enlace_3 = get_sub_field( 'enlace_3' ); ?>
-	      	<?php if ( $enlace_3 ) { ?>
-	        <div class="featured-text"><a href="<?php echo $enlace_3; ?>">
-	            <h2><?php the_sub_field( 'titulo_3' ); ?></h2></a>
-	        <?php } ?>
-	        </div>
-	      </div>
-	    </div>
-	  </div>
-	  <div class="col-md-7 text-center">
-	  	<?php if ( get_sub_field( 'imagen_4' ) ) { ?>
-	    <div class="feat-article"><img class="img-fluid feat-img" src="<?php the_sub_field( 'imagen_4' ); ?>" alt="<?php the_sub_field( 'titulo_4' ); ?>"/>
-	    <?php } ?>
-	      <div class="hover-featured">
-	        <?php $enlace_4 = get_sub_field( 'enlace_4' ); ?>
-	      	<?php if ( $enlace_4 ) { ?>
-	        <div class="featured-text"><a href="<?php echo $enlace_4; ?>">
-	            <h2><?php the_sub_field( 'titulo_4' ); ?></h2></a>
-	        <?php } ?>
-	        </div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
-	<?php endwhile; ?>
-	<?php endif; ?>
-	</div>
+	
 </section>
 <?php if ( have_rows( 'opciones_banda', 'option' ) ) : ?>
 <div class="container-fluid">
