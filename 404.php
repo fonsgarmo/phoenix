44a19f9a860f9c1593f0a842774c974b19704e18
Alfonso <fonxi1994@gmail.com>
2018-10-11 00:53:04 +0200

    Translation

--- 404.php
+++ 404.php
@@ -13,13 +13,13 @@ get_header();
 <div id="notfound-page" class="container">
 	<div class="row">
 		<div class="col-xl-auto">
-			<h1 class="archive-title"><span>404: no encontrado</span></h1>
+			<h1 class="archive-title"><span>404: not found</span></h1>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xl-12 text-center">
 			<img src="https://c2.staticflickr.com/2/1756/42897016091_919afb8b2f_o.png" alt="Error 404" class="img-fluid">
-			<h3>Shoo flee! No hemos podido encontrar la página a la que intentabas acceder. Utiliza el menú superior, <a href="<?php echo get_site_url(); ?>">vuelve al inicio</a> o usa el buscador para encontrar lo que buscas.</h3>
+			<h3>Shoo flee! We couldn't find the page you were trying to access. Use the main menu, <a href="<?php echo get_site_url(); ?>">go back to the home</a> or use the search box to find what you're looking for.</h3>
 		</div>	
 	</div>
 	<div class="row">
--- functions.php
+++ functions.php
@@ -232,7 +232,7 @@ add_action( 'widgets_init', 'custom_sidebars' );
 function wpdocs_my_search_form( $form ) {
     $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
     <div>
-    <input class="form-control" placeholder="Buscar..." type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input class="form-control" placeholder="Search..." type="text" value="' . get_search_query() . '" name="s" id="s" />
     </div>
     </form>';
  
@@ -243,7 +243,7 @@ add_filter( 'get_search_form', 'wpdocs_my_search_form' );
 function mobile_search_form( $form ) {
     $form = '<form role="search" method="get" id="searchform-mobile" class="searchform" action="' . home_url( '/' ) . '" >
     <div>
-    <input class="form-control" placeholder="Buscar..." type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input class="form-control" placeholder="Search..." type="text" value="' . get_search_query() . '" name="s" id="s" />
     </div>
     </form>';
  
