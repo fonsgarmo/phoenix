b962cb51347e63bdee514ea7770938bc0f8bf170
Alfonso <fonxi1994@gmail.com>
2018-10-28 15:16:21 +0100

    Dropdown menu

--- js/main.js
+++ js/main.js
@@ -12,12 +12,4 @@ $(document).ready(function(){
         $('#header-search-row').removeClass('show-search');
     });
 
-    $( "li.menu-item" ).hover(function() {  // mouse enter
-    $( this ).find( " > .sub-menu" ).show(); // display immediate child
-
-	}, function(){ // mouse leave
-	    if ( !$(this).hasClass("current_page_item") ) {  // check if current page
-	        $( this ).find( ".sub-menu" ).hide(); // hide if not current page
-	    }
-	});
 });
